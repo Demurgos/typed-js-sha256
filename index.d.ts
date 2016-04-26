4272f581034dd58c1e9e1cff794d933f40ee41d2
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-04-26 20:28:05 +0200

    Fix typings and add some tests

--- index.d.ts
+++ index.d.ts
@@ -19,14 +19,13 @@
  */
 
 /**
- * Returns the SHA-256 hash of `message` as an hexadecimal string. If `is224` is true, it returns a SHA-224 hash instead.
+ * Returns the SHA-256 hash of `message` as an hexadecimal string.
  * @param message The message to hash
- * @param is224 Return a SHA-224 hash instead of a SHA-256 hash
  */
-export function sha256(message: string | Array | Uint8Array | ArrayBuffer, is224?: boolean): string;
+export function sha256(message: string | ArrayLike<number> | Uint8Array | ArrayBuffer): string;
 
 /**
  * Returns the SHA-224 hash of `message` as an hexadecimal string.
  * @param message The message to hash
  */
-export function sha224(message: string | Array | Uint8Array | ArrayBuffer): string;
+export function sha224(message: string | ArrayLike<number> | Uint8Array | ArrayBuffer): string;
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,39 @@
-import test = require('blue-tape');
+import * as test from 'blue-tape';
 
-import jsSha256 = require('js-sha256');
+import {sha256, sha224} from 'js-sha256';
+
+test('sha256(string)', (t) => {
+  t.plan(3);
+  t.equal(sha256(''), 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+  t.equal(sha256('The quick brown fox jumps over the lazy dog'), 'd7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592');
+  t.equal(sha256('The quick brown fox jumps over the lazy dog.'), 'ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c');
+});
+
+test('sha256(Array<number>)', (t) => {
+  t.plan(2);
+  t.equal(sha256([]), 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+  t.equal(sha256([211, 212]), '182889f925ae4e5cc37118ded6ed87f7bdc7cab5ec5e78faef2e50048999473f');
+});
+
+test('sha256(Uint8Array)', (t) => {
+  t.plan(2);
+  t.equal(sha256(new Uint8Array([])), 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+  t.equal(sha256(new Uint8Array([211, 212])), '182889f925ae4e5cc37118ded6ed87f7bdc7cab5ec5e78faef2e50048999473f');
+});
+
+test('sha256(ArrayBuffer)', (t) => {
+  t.plan(1);
+  t.equal(sha256(new ArrayBuffer(0)), 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+});
+
+test('sha256(ArrayBuffer)', (t) => {
+  t.plan(1);
+  t.equal(sha256(new ArrayBuffer(0)), 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+});
+
+test('sha224(string)', (t) => {
+  t.plan(3);
+  t.equal(sha224(''), 'd14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f');
+  t.equal(sha224('The quick brown fox jumps over the lazy dog'), '730e109bd7a8a32b1cb9d9a09aa2325d2430587ddbc0c38bad911525');
+  t.equal(sha224('The quick brown fox jumps over the lazy dog.'), '619cba8e8e05826e9b8c519c0a5c68f4fb653e8a3d8aa04bb2c8cd4c');
+});
